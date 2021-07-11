367805bfa745df62f464e9657bccf874d6281c7a
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 12:35:54 -0400

    Add files via upload

---
+++ Lamp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    public Light lamp = null;
+    private string floor_name = "";
+
+    private void Start()
+    {
+        floor_name = this.transform.parent.name;
+        lamp.enabled = true;
+    }
+
+    //step 3. listen to event
+    private void OnEnable()
+    {
+        Player.OnPlayerFloorChange += DoLight;  //add listener
+    }
+    private void OnDisable()
+    {
+        Player.OnPlayerFloorChange -= DoLight;  //remove listener
+    }
+    private void DoLight(string Floorname)
+    {
+        if (Floorname == floor_name)
+        {
+            lamp.enabled = true;
+        }
+        else
+        {
+            lamp.enabled = false;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    //step 1. define my event
+    public delegate void PlayerFloorChange(string floorname);
+    public static event PlayerFloorChange OnPlayerFloorChange;
+
+    public float Movespeed = 3.5f;
+    public float Jumpforce = 8.0f;
+    private Rigidbody rb = null;
+    private GameObject currentFloor = null;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        //move control
+        float xval = Input.GetAxis("Horizontal");
+        float zval = Input.GetAxis("Vertical");
+        this.transform.Translate(Vector3.right * xval * Movespeed * Time.deltaTime);
+        this.transform.Translate(Vector3.forward * zval * Movespeed * Time.deltaTime);
+
+        //jump control
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            rb.AddRelativeForce(Vector3.up * Jumpforce, ForceMode.VelocityChange);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor") == true)
+        {
+            if (currentFloor == null || currentFloor.name != collision.gameObject.name)
+            {
+                // step 2. fire off my event
+                if (OnPlayerFloorChange != null)
+                {
+                    OnPlayerFloorChange(collision.gameObject.name);
+                } 
+            }
+        }
+    }
+}
